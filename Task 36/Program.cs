67827d8f25afccf7f2065ac5bfb097eb69396dc3
Alexfishkov <afscorp@mail.ru>
2022-12-15 17:34:38 +0300

    Sum of elements in odd positions

---
+++ Task 36/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] array = new int[10];
+
+int oddsum(int[] arr)
+{
+    int answer = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 != 0) answer += arr[i];
+    }
+    return answer;
+}
+
+Console.Clear();
+Console.WriteLine("Задан массив:");
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-99, 100);
+    Console.Write($"{array[i]} ");
+}
+
+Console.WriteLine();
+
+Console.WriteLine($"Сумма элементов, стоящих на нечётных позициях: {oddsum(array)}");
